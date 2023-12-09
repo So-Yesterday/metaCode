09becd2e1769d75d6b2578b0b60a5e0a4a9eef1a
YunFei <1572843061@qq.com>
2023-12-09 10:36:10 +0800

    add spring源码编译&测试成功

---
+++ Spring-YunFei/src/main/java/com/fei/config/JavaConfig.java
@@ -0,0 +1,20 @@
+package com.fei.config;
+
+
+import com.fei.beans.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan
+public class JavaConfig {
+
+	@Bean
+	public User user() {
+		return new User("YunFei", 18);
+	}
+
+
+}
---
+++ Spring-YunFei/src/test/java/SpringTest.java
@@ -0,0 +1,16 @@
+import com.fei.beans.User;
+import com.fei.config.JavaConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringTest {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+
+		User user = (User) context.getBean("user");
+
+		System.out.println(user);
+
+
+	}
+}
